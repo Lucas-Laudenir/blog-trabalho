0399873c7aca20afd6f36884d9af51616013114e
lucas <lucaslaudenirfilipim@gmail.com>
2024-11-05 18:59:47 -0300

    trazendo alterações do trabalho

---
+++ src/Auth/auth.controller.ts
@@ -0,0 +1,30 @@
+
+import { AuthService } from './auth.service';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Request,
+  UseGuards
+} from '@nestjs/common';
+import { AuthGuard } from './auth.guard';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @HttpCode(HttpStatus.OK)
+  @Post('login')
+  signIn(@Body() signInDto: Record<string, any>) {
+    return this.authService.signIn(signInDto.username, signInDto.password);
+  }
+
+  @Get('profile')
+  getProfile(@Request() req) {
+    return req.user;
+  }
+}
+
